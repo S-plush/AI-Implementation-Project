2d7a402842aef0cd61485144728db9aff84de4f1
S Plush <48722178+S-plush@users.noreply.github.com>
2025-10-18 14:46:27 -0400

    added player control

---
+++ Assets/Scripts/Enemy FOV.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFOV : MonoBehaviour
+{
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstacleMask;
+
+    [Range(0, 360)] public float angle;
+    public float radius;
+    public GameObject player;
+    public bool playerVisable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(.2f);
+
+        while (true)
+        {
+            yield return wait;
+
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if(rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    playerVisable = true;
+                }
+                else
+                {
+                    playerVisable = false;
+                }
+            }
+            else
+            {
+                playerVisable = false;
+            }
+        }
+        else if (playerVisable)
+        {
+            playerVisable = false;
+        }
+    }
+
+    public bool IsPlayerVisible()
+    {
+        return playerVisable;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+        Vector3 viewAngle1 = DirectionFromAngle(transform.eulerAngles.y, -angle / 2);
+        Vector3 viewAngle2 = DirectionFromAngle(transform.eulerAngles.y, angle / 2);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + viewAngle1 * radius);
+        Gizmos.DrawLine(transform.position, transform.position + viewAngle2 * radius);
+
+        if (playerVisable && player != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, player.transform.position);
+        }
+    }
+
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
---
+++ Assets/Scripts/Player Controls.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] private float movementSpd;
+
+    private CharacterController charController;
+    private float ySpeed;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = transform.forward * verticalInput;
+
+        if (horizontalInput > 0 || horizontalInput < 0)
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, horizontalInput * .5f, 0));
+        }
+
+        float magnitude = Mathf.Clamp01(moveDirection.magnitude) * movementSpd;
+        moveDirection.Normalize();
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (charController.isGrounded)
+        {
+            ySpeed = 0f;
+        }
+
+        Vector3 velocity = moveDirection * magnitude;
+        velocity = OnSlope(velocity);
+        velocity.y += ySpeed;
+        charController.Move(velocity * Time.deltaTime);
+    }
+
+    private Vector3 OnSlope(Vector3 velocity)
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, out RaycastHit slopeHit, .4f))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, slopeHit.normal);
+            var adjustVelocity = slopeRotation * velocity;
+
+            if(adjustVelocity.y < 0)
+            {
+                return adjustVelocity;
+            }
+        }
+
+        return velocity;
+    }
+}
